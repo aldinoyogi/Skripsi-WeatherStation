05d430978de0176eea34c944a09714bd123e626e
= <=>
2024-07-20 01:50:09 +0700

    Add Changes

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const path = require('path');
 
 
