b5b249102163482da489a826bda69e4130bea469
= <=>
2024-07-20 02:45:03 +0700

    Add Changes

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const path = require('path');
 
 
 
-const admin = require("firebase-admin");
-const serviceAccount = require("./weather-station.json");
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./weather-station.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://weathercast-3d11e-default-rtdb.asia-southeast1.firebasedatabase.app"
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://weathercast-3d11e-default-rtdb.asia-southeast1.firebasedatabase.app"
+// });
 
 
 
@@ -27,35 +27,40 @@ app.get('/', (_, res) => {
 
 const sockets = {};
 
-const realtime_db = admin.database();
-realtime_db.ref("Weather_Station_A/realtime").on("value", (snapshot) => {
-  const data = snapshot.val();
-  Object.keys(sockets).forEach((key) => {
-    const socket = sockets[key];
-    const message = {
-      type: 'realtime',
-      data
-    }
-    socket.emit("data", message);
-  })
-});
+// const realtime_db = admin.database();
+// realtime_db.ref("Weather_Station_A/realtime").on("value", (snapshot) => {
+//   const data = snapshot.val();
+//   Object.keys(sockets).forEach((key) => {
+//     const socket = sockets[key];
+//     const message = {
+//       type: 'realtime',
+//       data
+//     }
+//     socket.emit("data", message);
+//   })
+// });
 
 io.on('connection', (socket) => {
   sockets[socket.id] = socket;
-  realtime_db.ref("Weather_Station_A/realtime").get().then((snapshot) => {
-    if (snapshot.exists()) {
-      const data = snapshot.val();
-      const message = {
-        type: 'realtime',
-        data
-      }
-      socket.emit("data", message);
-    }
-  })
+  // realtime_db.ref("Weather_Station_A/realtime").get().then((snapshot) => {
+  //   if (snapshot.exists()) {
+  //     const data = snapshot.val();
+  //     const message = {
+  //       type: 'realtime',
+  //       data
+  //     }
+  //     socket.emit("data", message);
+  //   }
+  // })
 
-  socket.emit("data", { type: "history_perminutes", data: null })
+  // socket.emit("data", { type: "history_perminutes", data: null })
+  const sendMessage = setInterval(() => {
+    socket.emit("data", { type: "dummy", data: "Hello" })
+  }, 2000)
 
   socket.on('disconnect', () => {
+    clearInterval(sendMessage);
+    socket.disconnect();
     delete sockets[socket.id];
   });
 });
